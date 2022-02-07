d6e9ca66566e916c21e7a71309ecfa08bcd4e13c
ubiratanfilho <ubfilho16@gmail.com>
2022-02-07 15:55:30 -0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -3,9 +3,8 @@ import tensorflow as tf
 from PIL import Image
 
 def main():
-    st.header("How old are you according to a Cnvolution Neural Network? 🥸👧")
-    st.write("Upload an image of yourself below to find out! (preferably a squared image and containing only your face)")
-    file = st.file_uploader("Upload Photo")
+    st.header("How old are you according to a Convolution Neural Network? 🥸👧")
+    file = st.file_uploader("Upload an image of yourself below to find out! (preferably a squared image and containing only your face)")
     if file is not None:
         st.image(file, width=300)
         image = Image.open(file)
